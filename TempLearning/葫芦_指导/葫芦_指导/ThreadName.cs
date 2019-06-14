d6461d57a8151947553c73c0b7f619050948d8b6
qq651267797 <feijiumeng@163.com>
2019-06-14 19:18:29 +0800

    2019年6月14日19:18:19

---
+++ TempLearning/葫芦_指导/葫芦_指导/ThreadName.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadName
+{
+    class ThreadName
+    {
+        static void Main(string[] args)
+        {
+            Thread MyThread = Thread.CurrentThread;
+
+            MyThread.Name = "xuegao";
+
+            Console.WriteLine("This is " + MyThread.Name);
+            Console.ReadKey();
+        }
+    }
+}
